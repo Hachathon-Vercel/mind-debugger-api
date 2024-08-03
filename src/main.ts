28a9faf85e947c92548639b756e1e62d28a1c164
95caparra <caparra95@misena.edu.co>
2024-08-03 09:42:58 -0500

    https

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,27 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 import * as bodyParser from 'body-parser';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+
+  const httpsOptions = {
+    key: fs.readFileSync('./src/cert/key.pem'),
+    cert: fs.readFileSync('./src/cert/cert.pem'),
+  };
+
+
+  const app = await NestFactory.create(
+    AppModule, 
+    { httpsOptions },
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
+    
   );
 
   app.enableCors();
